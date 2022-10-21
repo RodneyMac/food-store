bbdac7c0020f7fa11a32d1879d4df4bb09c80568
RodneyMac <rodneydavidmaciel@gmail.com>
2022-10-21 11:09:44 -0300

    redux store and slices created, react-icons installed

---
+++ client/src/app/store.js
@@ -0,0 +1,17 @@
+import {configureStore} from "@reduxjs/toolkit";
+import productsReducer, { productsFetch } from "../features/productsSlice";
+import { productsApi } from "../features/productsApi";
+import cartReducer, { getTotals } from "../features/cartSlice";
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    cart: cartReducer,
+    [productsApi.reducerPath] : productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware)
+});
+
+store.dispatch(productsFetch());
+store.dispatch(getTotals());
