7b8dc56ba2c9d78fb51b71e8c3d49445eccf26eb
RodneyMac <rodneydavidmaciel@gmail.com>
2022-10-21 10:47:13 -0300

    backend created, and client folder started

---
+++ backend/products.js
@@ -0,0 +1,92 @@
+const products = [
+  {
+    id: 1,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1665616990/muzzarella_mkapoi.webp",
+    title: "Muzzarella",
+    description: "Queso muzzarella y aceitunas",
+    price: 400,
+    category: "Pizzas"
+  },
+  {
+    id: 2,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198766/napolitana_upssdj.webp",
+    title: "Napolitana",
+    description: "Tomate, morrón, queso y aceitunas",
+    price: 400,
+    category: "Pizzas"
+  },
+  {
+    id: 3,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198766/jamonymorron_li2xn9.webp",
+    title: "Jamón y Morrón",
+    description: "Jamón, morrón, queso y aceitunas",
+    price: 400,
+    category: "Pizzas"
+  },
+  {
+    id: 4,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198765/empanada_atun_leib7o.webp",
+    title: "Empanada de Atún",
+    description: "Atún, verduras",
+    price: 120,
+    category: "Empanadas"
+  },
+  {
+    id: 5,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198765/empanada_carne_hfgqgy.webp",
+    title: "Empanada de Carne",
+    description: "Carne y verduras",
+    price: 120,
+    category: "Empanadas"
+  },
+  {
+    id: 6,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198766/empanada_jamonyqueso_zf45mc.webp",
+    title: "Empanada de jamón y queso",
+    description: "Jamón, queso, salsa blanca",
+    price: 120,
+    category: "Empanadas"
+  },
+  {
+    id: 7,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198765/agua_uitdjl.webp",
+    title: "Agua",
+    description: "Agua mineral sin gas 400 ml",
+    price: 200,
+    category: "Bebidas"
+  },
+  {
+    id: 8,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198766/gaseosa_jthbw2.webp",
+    title: "Gaseosa",
+    description: "Sabores variados 400 ml",
+    price: 250,
+    category: "Bebidas"
+  },
+  {
+    id: 9,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198765/cerveza_wfaldj.webp",
+    title: "Cerveza",
+    description: "En lata 400 ml",
+    price: 300,
+    category: "Bebidas"
+  },
+  {
+    id: 10,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198765/bombones_hxgumm.webp",
+    title: "Bombones",
+    description: "Chocolate, menta",
+    price: 300,
+    category: "Postres"
+  },
+  {
+    id: 11,
+    imgUrl: "https://res.cloudinary.com/de1xrxlcz/image/upload/v1666198766/helado_kkmjni.webp",
+    title: "Helado",
+    description: "Variedad de sabores",
+    price: 150,
+    category: "Postres"
+  }
+];
+
+module.exports = products;
