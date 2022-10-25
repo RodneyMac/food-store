f09469001dd06772f1a1f30fc420bf2dd75bbb1c
RodneyMac <rodneydavidmaciel@gmail.com>
2022-10-24 22:04:55 -0300

    checkout form added

--- client/src/views/Checkout.js
+++ client/src/views/Checkout.js
@@ -1,38 +1,106 @@
-import React from 'react';
-import { paidProduct } from '../features/cartSlice';
-import { useDispatch } from 'react-redux';
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { paidProduct } from "../features/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 const Checkout = () => {
-  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
 
-  const handlePaidProduct = () => {
+  const generateId = () => {
+    let generate = Math.floor(Math.random() * 1000);
+    return generate;
+  };
+
+  const [clientData, setClientData] = useState([]);
+  const [customer, setCustomer] = useState({});
+
+  const handleChange = (e) => {
+    setCustomer({
+      ...customer,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handlePaidProduct = (e) => {
+    e.preventDefault();
+    let newUser = { ...customer, id: generateId() };
+    setClientData((state) => [...state, newUser]);
+    setCustomer({});
+    console.log(clientData);
+    console.log(`Total compra: ${cart.cartTotalAmount}`, newUser);
+    // console.log(`Productos: ${cart.cartItems}`);
+
     dispatch(paidProduct());
+    navigate("/confirmacion-de-compra");
   };
 
   return (
-    <div className='container d-flex flex-column justify-content-center align-items-center'>
-      <h2 className='text-center mt-4 text-info'>Checkout</h2>
+    <div className="container d-flex flex-column justify-content-center align-items-center">
+      <h2 className="text-center mt-4 text-info">Checkout</h2>
       <h2 className="text-info text-center">Pago en efectivo con delivery</h2>
-      <div className='w-50'>
+      <div className="d-flex justify-content-center align-items-center mb-3">
+        <h4 className="text-info">Total: </h4>
+        <h4 className="text-warning mx-2">$ {cart.cartTotalAmount}</h4>
+        <div className="text-white">{cart.cartItems.map((item) => (
+          <div className="text-info" key={item.id}>{item.title}</div>
+        ))}</div>
+      </div>
+      <div className="w-50">
         <div className="mb-3">
-          <input type="number" className="form-control" id="telefono" placeholder="Teléfono" autoFocus/>
+          <input
+            type="number"
+            className="form-control"
+            id="telefono"
+            name="telefono"
+            placeholder="Teléfono"
+            autoFocus
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-3">
-          <input type="text" className="form-control" id="nombre" placeholder="Nombre"/>
+          <input
+            type="text"
+            className="form-control"
+            id="nombre"
+            name="nombre"
+            placeholder="Nombre"
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-3">
-          <input type="text" className="form-control" id="direccion" placeholder="Dirección"/>
+          <input
+            type="text"
+            className="form-control"
+            id="direccion"
+            name="direccion"
+            placeholder="Dirección"
+            onChange={handleChange}
+          />
         </div>
         <div className="mb-3">
-          <textarea className="form-control" id="comentarios" rows="3"></textarea>
+          <textarea
+            className="form-control"
+            id="comentarios"
+            rows="3"
+            name="comentarios"
+            onChange={handleChange}
+          ></textarea>
         </div>
-        <div className='text-center'>
-          <Link to="/confirmacion-de-compra" className='btn btn-primary' onClick={handlePaidProduct}>Confirmar Pago</Link>
+        <div className="text-center">
+          <button
+            className="btn btn-primary"
+            onClick={handlePaidProduct}
+          >
+            Confirmar Pago
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Checkout;
