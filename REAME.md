e5bc60d981624cbe4f964bb029fe24d06571bce2
ArceDaniel <jonathandanielarce9@gmail.com>
2022-10-25 21:33:28 -0300

    update of routes

---
+++ REAME.md
@@ -0,0 +1,33 @@
+PROYECTO: PIZZERÍA DON REMOLO
+
+Menú de comidas para realizar pedidos online. Gestor de pedidos para el local. #idea3
+
+Descripción del proyecto:
+
+Se requiere un menú intuitivo y simple de usar para realizar pedidos vía whatsapp.
+
+Objetivo:
+
+El usuario final debe poder armar su pedido online con todos los ítems agregados, conocer su total, y recibir confirmación del local.
+
+Requisitos:
+
+-Los usuarios no necesitan estar registrados. -Se debe poder visualizar el listado de comidas ofrecidas separadas en categorías: Pizzas, Empanadas, Bebidas, Postres. -El usuario debe poder agregar X cantidad de cada item en su pedido. -El pedido puede realizar su checkout sin pago ni envío de por medio. Se debe aclarar que es sólo en efectivo y con delivery propio del negocio. -En el checkout se crea un texto automático que se envía al whatsapp del local.
+
+Páginas a desarrollar:
+
+-Listado de comidas -Vista del pedido -Confirmación del pedido
+
+TRABAJO PARA UX: Research y prototipado.
+
+TRABAJO PARA DISEÑO/UI: Diseñar cada página.
+
+TRABAJO PARA FRONTEND: Maquetación de cada página.
+
+TRABAJO PARA BACKEND: Creación de la base de datos. Creación de la lógica y control de los datos. (API o con motor de plantillas)
+
+TRABAJO PARA QA/TESTING: Crear tests para validar los errores en cada página.
+
+TRABAJO PARA DEVOPS: Crear un deploy para 3 ambientes: Dev, Staging y Production. Al terminar:
+
+Suma el proyecto a tu portfolio. Comparte tu trabajo y ayuda a otros a sumar tu aporte utilizando el hashtag #idea3 Cuenta tu experiencia en las redes para demostrar tus logros
--- backend/router/order.routes.js
+++ backend/router/order.routes.js
@@ -5,51 +5,39 @@ import OrderModel from '../schemas/orderSchema.js';
 const orderRouter = Router();
 
 orderRouter.post("/", async(req, res) => {
-  const {orden, address, phone} = req.body;
-
-  const finalPrice =200;
-  const order = new OrderModel({orden, address,phone ,price:finalPrice});
-  
+  const {id ,orden, address, phone,price} = req.body;
+  const order = new OrderModel({orden, address,phone ,price});
   await order.save();
-
-  return res.sendStatus(201);
+  return res.sendStatus(200);
 });
 
-// orderRouter.put();
-// orderRouter.delete();
+orderRouter.patch("/", async(req, res) => {
+  const { id } = req;
+    const { orden, address, phone,price } = req.body;
 
-// orderRouter.get();
-// orderRouter.get();
+    const existingOrder = await OrderModel.findById(id).exec();
+    if (!existingOrder)
+        return res.status(401).send({ errors: ['pedido no existente'] });
+
+    existingOrder.orden = orden;
+    existingOrder.address = address;
+    existingOrder.phone = phone;
+    existingOrder.price = price;
+
+    await existingOrder.save();
+
+    return res.send('Orden actualizado');
+});
 
 
-// // ver el pedido
-
-// orderRouter.get('/', async (req, res) => {
-//   const orders = await Order.find();
-//   res.json(orders);
-// });
-
-// 
 orderRouter.get('/:id', async (req, res) => {
   const order = await OrderModel.findById(req.params.id);
   res.json(order);
 });
 
-// // hacer pedido
-
-// orderRouter.post('/',async(req, res) => {
-//     const { __id, orden, address, price } = req.body;
-//     const order = new OrderModel({ __id, orden, address, price});
-//     await order.save();
-//     return res.sendStatus(201); 
-//   });          
-
-
-// cancelar pedido
 orderRouter.delete('/:id', async (req, res) => {
   await OrderModel.findByIdAndRemove(req.params.id);
   res.json({status: 'order deleted'});
 });
-
 
 export default orderRouter;
